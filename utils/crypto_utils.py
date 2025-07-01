1373998655d2aba0af8a75003283d4d8ab7002de
hershey13 <110694385+hershey13@users.noreply.github.com>
2025-07-01 20:49:33 +0530

    Create crypto_utils.py

---
+++ utils/crypto_utils.py
@@ -0,0 +1,7 @@
+def encrypt_message(message, password, private_key, otp):
+    # This is just a mock encryption function
+    return f"ENCRYPTED({message}) WITH ({password}, {private_key}, {otp})"
+
+def decrypt_message(encrypted_data, private_key, otp):
+    # This is just a mock decryption function
+    return "SIMULATED_ORIGINAL_MESSAGE"
